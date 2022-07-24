d370d7b15359f70849dfb1d928b825cbd50f2091
jpcp0614 <jpcp0614@gmail.com>
2022-07-24 03:42:08 -0300

    :sparkles: feat: Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+from tech_news.analyzer.formatted import formatted
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    new_list = find_news()
+    if new_list is None:
+        return None
+
+    new_list.sort(
+        key=lambda new: new['comments_count'],
+        reverse=True)
+
+    return formatted(new_list[:5])
 
 
 # Requisito 11

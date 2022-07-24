c4d083bc5858ae23fbb6ab700e1133ef6c96bb3f
jpcp0614 <jpcp0614@gmail.com>
2022-07-24 03:32:58 -0300

    :sparkles: feat: Requisito 09

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -28,4 +28,5 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    data = search_news({"category": {"$regex": category, "$options": "i"}})
+    return formatted(data)
